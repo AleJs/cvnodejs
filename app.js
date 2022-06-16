7ad23f1570aee717faf16aa1e220d737fcec57d0
Alejandro ramirez <alejscript@gmail.com>
2022-06-16 17:03:15 -0300

    cv

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 2000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.use('/pdf', express.static(__dirname + '/cvfrontendalejandro.pdf'));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
